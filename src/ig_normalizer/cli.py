b589e9d25d19797b30a8a31d52be28a1dbca801c
GuilhermeVPX3 <guilherme@igpsp.com>
2026-04-18 16:07:28 -0300

    chore: bump version to 1.0.4

--- src/ig_normalizer/cli.py
+++ src/ig_normalizer/cli.py
@@ -92,7 +92,7 @@ Examples:
     parser.add_argument(
         "--version",
         action="version",
-        version="ig-normalizer 1.0.3",
+        version="ig-normalizer 1.0.4",
     )
 
     return parser
